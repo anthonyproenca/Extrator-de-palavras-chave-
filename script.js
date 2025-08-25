8ca01e626194cd7ec2a4601198861b549e7bc174
anthonyproenca <anthony.proenca@escola.pr.gov.br>
2025-08-25 09:02:40 -0300

    fgr

---
+++ script.js
@@ -0,0 +1,11 @@
+cost botaoMostraPalavras= document.query Selector('#botao-palavrachave');
+botaoMostraPalavra.addEventListener('click',mostraPalavrasChaves);
+function mostraPalavrasChaves()]{
+    cost texto =document.querySelector('#entrada-de-texto').value;
+    cost campoResultado=document.querySelector('#resultado-palavrachave');
+    cost palavraChave=processaTexto(texto);
+    campoResultado.textContent=mostraPalavrasChaves.join(',');
+}
+function processaTexto(texto){
+    let palavras = texto.split(/\P{L}+/u);
+    return palavras
