0f38916979c280437a09780f23884c3687c94199
anthonyproenca <anthony.proenca@escola.pr.gov.br>
2025-09-15 08:59:23 -0300

    m, nm

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-cost botaoMostraPalavras= document.query Selector('#botao-palavrachave');
-botaoMostraPalavra.addEventListener('click',mostraPalavrasChaves);
-function mostraPalavrasChaves(){
-    cost texto =document.querySelector('#entrada-de-texto').value;
-    cost campoResultado=document.querySelector('#resultado-palavrachave');
-    cost palavraChave=processaTexto(texto);
-    campoResultado.textContent=mostraPalavrasChaves.join(',');
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
+
+function mostraPalavrasChave() {
+  const texto = document.querySelector("#entrada-de-texto").value;
+
+  const campoResultado = document.querySelector("#resultado-palavrachave");
+
+  const palavrasChave = processaTexto(texto);
+
+  campoResultado.textContent = palavrasChave.join(", ");
 }
-function processaTexto(texto){
-    let palavras = texto.split(/\P{L}+/u);
-    return palavras
+
+function processaTexto(texto) {
+  let palavras = texto.split(/\P{L}+/u); 
+
+  return palavras;
 }
